78ee2fd23bcaec1d798dd16099915a4a83eabced
jbruns <jbruns@live.com>
2025-06-10 15:17:23 -0700

    qbittorrent: hardcode category path

--- tn/media/qbittorrent/copy_complete.sh
+++ tn/media/qbittorrent/copy_complete.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 CATEGORY="$1"
 SOURCE_DIR="$2"
-DEST_DIR="$3"
 
 if [ "$CATEGORY" = "abr-prowlarr" ]; then
-    cp -r "$SOURCE_DIR" "$DEST_DIR"
+    cp -r "$SOURCE_DIR" /data/audiobooks/
 fi
